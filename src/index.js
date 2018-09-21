ea3390e0d6fa3b88ac3ec9a26acb36ce85bad91c
Aleksandr2102 <aleksschpak@gmail.com>
2018-09-21 09:12:55 +0300

    'quadratic-equation'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  var arr = equation.split(' ');
+  var a = arr[0];
+  var b = arr[3] + arr[4];
+  var c = arr[7] + arr[8];
+  var D = b * b - 4 * a * c;
+  var endArr = [];
+  if (D > 0) {
+  	endArr.push(Math.round(((-b) + Math.sqrt(D)) / (2 * a)));
+  	endArr.push(Math.round(((-b) - Math.sqrt(D)) / (2 * a)));
+  }
+
+endArr.sort(function(a, b) { return a - b; });
+return endArr;
 }
